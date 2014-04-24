a893ce4c689fc43cd8cb2bc2d4cde94a5457dc3c
davidpablos <david.pablos@gmail.com>
2014-04-24 08:37:42 +0200

    Corrección DNI

--- EjerciciosJava/src/com/davidpablos/estandar/PruebaDNI.java
+++ EjerciciosJava/src/com/davidpablos/estandar/PruebaDNI.java
@@ -7,7 +7,7 @@ public class PruebaDNI {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ValidadorDNI validadorDNI = new ValidadorDNI("71449801W");
+		ValidadorDNI validadorDNI = new ValidadorDNI("80611315H");
 		System.out.println(validadorDNI.validarDNI());
 	}
 
